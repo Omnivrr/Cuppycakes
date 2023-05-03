47580bbb0c6607c07ab0811529838601a3a8ed84
Omnivrr <Omnivrr@gmail.com>
2023-05-03 15:10:39 +0300

    Initial Commit

---
+++ Cuppycakes/CuppycakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuppycakesApp.swift
+//  Cuppycakes
+//
+//  Created by Bukhari Sani on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CuppycakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
